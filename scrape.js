6be1534c79a3ea21922f1787774dc020fc9ab533
Bryce Wong <super.bryce@gmail.com>
2019-01-31 14:46:55 -0500

    Just trying to figure out how to open the replies

--- scrape.js
+++ scrape.js
@@ -1,5 +1,6 @@
 var url ='https://www.webtoons.com/en/challenge/tested/heck-of-a-start/viewer?title_no=231173&episode_no=1';
-var page = new WebPage();
+var webPage = require('webpage');
+var page = webPage.create();
 var fs = require('fs');
 
 page.open(url, function (status) {
@@ -8,8 +9,13 @@ page.open(url, function (status) {
 
 function just_wait() {
     setTimeout(function() {
-               fs.write('comments.html', page.content, 'w');
+            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
+                page.evaluate(function(){
+                  $('u_cbox_btn_reply').click();
+                });
+            fs.write('comments.html', page.content, 'w');
             phantom.exit();
+            });
     }, 2500);
 }
 
