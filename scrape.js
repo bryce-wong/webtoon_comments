61adf644233d63fe0435bb0d648891fa65e5f9d7
Bryce Wong <super.bryce@gmail.com>
2019-02-04 17:56:32 -0500

    Trying to click on things!!! not working???

--- scrape.js
+++ scrape.js
@@ -3,11 +3,28 @@ var webPage = require('webpage');
 var page = webPage.create();
 var fs = require('fs');
 
+function click(el){
+    var ev = document.createEvent('MouseEvent');
+    ev.initMouseEvent(
+        'click',
+        true /* bubble */, true /* cancelable */,
+        window, null,
+        0, 0, 0, 0, /* coordinates */
+        false, false, false, false, /* modifier keys */
+        0 /*left*/, null
+    );
+    el.dispatchEvent(ev);
+}
+
 page.open(url, function (status) {
-        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
+              page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
                 page.evaluate(function(){
-                  document.querySelector('u_cbox_btn_reply').click();
+                  var replyboxes = document.getElementsByClassName('.u_cbox_btn_reply');
+                  for(var i = 0, j = replyboxes.length; i < j; i++){
+                    replyboxes[i].click();
+                  };
                 });
+        });
         just_wait();
 });
 
@@ -15,7 +32,6 @@ function just_wait() {
     setTimeout(function() {
             fs.write('comments.html', page.content, 'w');
             phantom.exit();
-            });
     }, 2500);
 }
 
