7f4eed959147e3772ca71bb2f0dcd69ceed88e67
Bryce Wong <super.bryce@gmail.com>
2019-02-04 15:00:57 -0500

    Trying a different way to click the button - also looked at RSelenium briefly.

--- scrape.js
+++ scrape.js
@@ -4,15 +4,15 @@ var page = webPage.create();
 var fs = require('fs');
 
 page.open(url, function (status) {
+        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
+                page.evaluate(function(){
+                  document.querySelector('u_cbox_btn_reply').click();
+                });
         just_wait();
 });
 
 function just_wait() {
     setTimeout(function() {
-            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
-                page.evaluate(function(){
-                  $('u_cbox_btn_reply').click();
-                });
             fs.write('comments.html', page.content, 'w');
             phantom.exit();
             });
