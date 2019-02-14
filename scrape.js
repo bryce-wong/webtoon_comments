cfe876a4958651cd1047b6d2d4720dc50dc77082
Bryce Wong <super.bryce@gmail.com>
2019-02-14 14:01:49 -0500

    Pretty much figured out how to do it all, just cleaning it up and figuring out how to automate going through all comments and all pages. Next step will be figuring out how to grab all the comments.

--- scrape.js
+++ scrape.js
@@ -3,26 +3,12 @@ var webPage = require('webpage');
 var page = webPage.create();
 var fs = require('fs');
 
-function click(el){
-    var ev = document.createEvent('MouseEvent');
-    ev.initMouseEvent(
-        'click',
-        true /* bubble */, true /* cancelable */,
-        window, null,
-        0, 0, 0, 0, /* coordinates */
-        false, false, false, false, /* modifier keys */
-        0 /*left*/, null
-    );
-    el.dispatchEvent(ev);
-}
-
 page.open(url, function (status) {
               page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function(){
                 page.evaluate(function(){
-                  var replyboxes = document.getElementsByClassName('.u_cbox_btn_reply');
-                  for(var i = 0, j = replyboxes.length; i < j; i++){
-                    replyboxes[i].click();
-                  };
+                  $(document).ready(function(){
+                    $('.u_cbox_btn_reply')[0].click();
+                  });
                 });
         });
         just_wait();
